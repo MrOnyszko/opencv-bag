721e90c9382a3a7bf8e8c1a66ce5188599f3ab24
Slawomir Onyszko <slawomir.onyszko@gmail.com>
2017-03-12 17:21:53 +0100

    Add canny edges detection example image

--- src/main/kotlin/pl/gratitude/opencvbag/Main.kt
+++ src/main/kotlin/pl/gratitude/opencvbag/Main.kt
@@ -1,9 +1,22 @@
 package pl.gratitude.opencvbag
 
+import org.bytedeco.javacpp.opencv_core.IPL_DEPTH_8U
+import org.bytedeco.javacpp.opencv_core.IplImage
+import org.bytedeco.javacpp.opencv_imgproc.*
+
 /**
  * @author Sławomir Onyszko
  */
 fun main(args: Array<String>) {
     val grabber = Grabber()
-    grabber.grabIt({ it }, 1)
+
+    val threshold: Double = 40.0
+    val apertureSize: Int = 3
+
+    grabber.grabIt({
+        val image: IplImage = IplImage.create(it.width(), it.height(), IPL_DEPTH_8U, 1)
+        cvCvtColor(it, image, CV_BGR2GRAY)
+        cvCanny(image, image, threshold, (threshold * 3), apertureSize)
+        image
+    }, 1)
 }
